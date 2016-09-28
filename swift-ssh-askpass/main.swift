ac2f49586e66f285a87ff3fa63573f2de29f1fe6
Jeremy Beker <gothmog@confusticate.com>
2016-09-28 16:57:47 -0400

    First pass.

--- swift-ssh-askpass/main.swift
+++ swift-ssh-askpass/main.swift
@@ -7,6 +7,34 @@
 //
 
 import Foundation
+import Cocoa
 
-print("Hello, World!")
+let application = NSApplication.shared()
+NSApp.setActivationPolicy(.accessory)
+NSApp.activate(ignoringOtherApps: true)
 
+let arguments = CommandLine.arguments
+guard arguments.count > 1 else { exit(1) }
+let message = arguments[1]
+
+let alert = NSAlert()
+
+alert.window.title = "Enter your passphrase"
+alert.messageText = "SSH Agent"
+alert.informativeText = message
+alert.icon = NSImage(named: NSImageNameCaution)
+alert.alertStyle = .warning
+
+let ok = alert.addButton(withTitle: "OK")
+let cancel = alert.addButton(withTitle: "Cancel")
+
+let passwordField = NSSecureTextField(frame: NSRect(x: 0, y: 0, width: 200, height: 48))
+
+alert.accessoryView = passwordField
+
+switch (alert.runModal()) {
+case 1000:
+    print(passwordField.stringValue)
+default:
+    break
+}
